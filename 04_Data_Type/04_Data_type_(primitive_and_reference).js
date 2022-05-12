ff976033a5cc74501000ba61ea2f7ef1d1d1f231
SanjayPaul4u <98152041+SanjayPaul4u@users.noreply.github.com>
2022-05-12 12:23:37 +0530

    this is first commit on this file
    no optional commit

---
+++ 04_Data_Type/04_Data_type_(primitive_and_reference).js
@@ -0,0 +1,53 @@
+// // Primitive data type📌📌📌  
+
+// String
+let Name = "sanjoy";
+console.log("The name is: "+Name);
+console.log((typeof Name));
+
+// Number
+let age =19;
+console.log("The age is: "+age);
+console.log((typeof age));
+
+// boolean
+let isDrive = true;
+console.log("can i drive : "+isDrive);
+console.log((typeof isDrive));
+
+// null
+let nVariable = null;
+console.log("The name2 is: "+nVariable);
+console.log((typeof nVariable));
+
+// undefined
+let name2 = undefined;
+console.log("The name2 is: "+name2);
+console.log((typeof name2));
+
+// // reference datatype📌📌📌 
+
+// // array
+let myArr = [45, true, 45.88, "sanjayPaul"];
+console.log(myArr);
+console.log((typeof myArr ));
+
+// // object literals
+let employee = {
+    Name : "sanjay",
+    age : 45,
+    'salary of emp' : 1
+}
+console.log(employee);
+console.table(employee);
+console.log((typeof(employee)));
+
+// // functions
+function func (){
+
+}
+console.log((typeof func));
+
+//// dates
+let date = new Date();
+console.log((typeof date));
